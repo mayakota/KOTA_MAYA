f58bf7475b15c349d652d2a46554bdfe7eb00fb6
mayakota <mkota99@gmail.com>
2016-09-29 10:19:27 -0700

    changed parts of the code

---
+++ Lesson_03/Lesson_03ex1.py
@@ -0,0 +1,24 @@
+name = input("What is your name?")
+
+print(name , "?! That's a terrible name!")
+
+age = input("How old are you?")
+
+print(age , "! That's really old!")
+
+fun = input("What do you do for fun?")
+
+print(fun , "Wow. Sounds pretty boring.")
+
+music = input("What kind of music do you like?")
+
+print("I hate that kind of music.")
+
+siblings = input("How many siblings do you have?")
+
+print(siblings , "well..That sounds painful.")
+
+growup = input("What do you want to be when you grow up?")
+
+print(growup , "? Ummm. I think you have to be smarter to be that.")
+
---
+++ Lesson_03/lesson_03ex2.py
@@ -0,0 +1,8 @@
+height = int(input("What is your height?"))
+weight = int(input("What is your weight?"))
+
+BMI = 703 * (weight / (height * height))
+
+print("Your BMI is", BMI,".")
+
+
