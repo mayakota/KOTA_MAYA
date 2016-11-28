112254edf8d8d7710e1dc972c0979224a8195c65
mayakota <mkota99@gmail.com>
2016-11-28 10:23:51 -0800

    Finished Lab_08

--- Lesson_08/TreeDeg60.py
+++ Lesson_08/TreeDeg60.py
@@ -1,11 +1,11 @@
 word = input("Please enter a word: ")
 stop = len(word)
-
+start = 0
 
 def tree(word, start, stop):
     if start <= stop:
-        print(0, start)
-        start = 1 + start
-        return tree(tree(word, start, stop))
+        print("{:>20}".format(word[0:start]))
+        start = start + 1
+        tree(word, start, stop)
 
-print(tree(word, start, stop))
+tree(word, start, stop)
