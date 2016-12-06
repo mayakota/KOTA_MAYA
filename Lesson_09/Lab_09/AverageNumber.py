5fc2038ce31ab3807162c167245123d635350d8d
mayakota <mkota99@gmail.com>
2016-12-06 10:21:34 -0800

    lesson9

--- Lesson_09/Lab_09/AverageNumber.py
+++ Lesson_09/Lab_09/AverageNumber.py
@@ -1,7 +1,18 @@
 numbers = []
 import random
-for i in range (1, 100):
-
+for i in range (0, 10):
+    numbers.append(random.randint(1, 100))
 print("Numbers...")
-
 output = ""
+for i in numbers:
+    output += str(i) + " "
+print(output)
+print(" ")
+
+def average(nums):
+    sum = 0
+    for i in nums:
+        sum += 1
+    return sum/len(nums)
+print("The average of the above numbers is...", average(numbers))
+    
---
+++ Lesson_09/New Text Document.py
@@ -0,0 +1,13 @@
+myList = [1, 2, 3, 4, 5]
+
+print(myList[2])
+
+print(myList[0:2])
+print(myList[2:3])
+print(myList)
+
+output = ""
+for i in myList:
+    output += str(i)
+
+print(output)
