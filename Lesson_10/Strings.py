de3488b0473c2036f3386ed3341059dc2fbeeb2c
mayakota <mkota99@gmail.com>
2017-01-06 10:10:17 -0800

    Lesson_10

---
+++ Lesson_10/Strings.py
@@ -0,0 +1,18 @@
+go = input("Please enter 16 strings: ")
+
+spList = go.split(" ")
+print(spList)
+
+wordsList = []
+
+spot = 0
+
+for i in range(0, 4):
+    output = ""
+    wordsList.append([])
+    for j in range(0, 4):
+        wordsList[i].append(spList[spot])
+        output += " " + wordsList[i][j]
+        spot += 1
+    print(output)
+    
