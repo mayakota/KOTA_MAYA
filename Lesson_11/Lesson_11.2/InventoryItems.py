4e53cdf8e234cfdf42bc2f275cf472faeedd1521
mayakota <mkota99@gmail.com>
2017-01-27 10:06:49 -0800

    11.2

--- Lesson_11/Lesson_11.2/InventoryItems.py
+++ Lesson_11/Lesson_11.2/InventoryItems.py
@@ -1,10 +1,10 @@
 import random
 class Item:
-    def __init__(self, iManufacturer, iName, iCategory = "", iPrice = ""):
-        self.itemManufacturer = iManufacturer
-        self.itemName = iName
-        self.itemCategory = iCategory
-        self.itemPrice = iPrice
+    def __init__(self, tManufacturer, tName, tCategory = "", tPrice = ""):
+        self.itemManufacturer = tManufacturer
+        self.itemName = tName
+        self.itemCategory = tCategory
+        self.itemPrice = tPrice
         self.userUPC = random.randint(0, 1000000)
 
     def setTHING(self, newItemManufacturer, newitemName, newitemCategory, newitemPrice):
@@ -26,7 +26,29 @@ class Item:
 
     def getItemPrice(self):
         return self.itemPrice
+
+    def getUserUPC(self):
+        return self.userUPC
     
     def __str__(self):
-           print("Customer Info...\nItem Manufacturer: ", self.itemManufacturer, "\nItem Name: ", self.lastName, "\nAvatar: ", self.avatar, "\nUser ID#: ", str(self.userID))
+           print("Customer Info...\nItem Manufacturer: ", self.itemManufacturer, "\nItem Name: ", self.itemName, "\nItem Category: ", self.itemCategory, "\nItem Price: ", self.itemPrice, "\nUser Price: ", self.userUPC)
+
+def main():
+    tName = input("Please enter the item's name: ")
+    tManufacturer = input("Please enter the item's manufacturer: ")
+    info = input("Will you be entering category and price information?")
+    if info == "n":
+        item1 = Item(iName, iManufacturer)
+    else:
+        tCategory = input("Please enter the item's category: ")
+        tPrice = int(input("Please enter the item's price: "))
+        item1 = Item(tManufacturer, tName, tCategory, tPrice)
+
+    print(item1.__str__())
+
+main()
+            
+        
+        
+                    
     
