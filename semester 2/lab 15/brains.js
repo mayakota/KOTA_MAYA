e494eef78efcc970d6041f4827c43a2559e698d5
mayakota <KotaM0993@sduhsd.lan>
2017-04-26 10:24:19 -0700

    15

--- semester 2/lab 15/brains.js
+++ semester 2/lab 15/brains.js
@@ -9,7 +9,24 @@ function shapes()
     canvas.fillStyle = g;
     canvas.beginPath();
     canvas.moveTo(200, 0);
-    canvas.lineTo(170. 130);
-    c
+    canvas.lineTo(170, 130);
+    canvas.lineTo(50, 50);
+    canvas.lineTo(130, 170);
+    canvas.lineTo(0, 200);
+    canvas.lineTo(130, 230);
+    canvas.lineTo(50, 350);
+    canvas.lineTo(170, 270);
+    canvas.lineTo(200, 400);
+    canvas.lineTo(230, 270);
+    canvas.lineTo(350, 350);
+    canvas.lineTo(270, 230);
+    canvas.lineTo(400, 200);
+    canvas.lineTo(270, 170);
+    canvas.lineTo(350, 50);
+    canvas.lineTo(230, 130);
+    canvas.closePath();
+    canvas.fill();
 
 }
+
+window.addEventListener("load", shapes, false);
