393dab39864a8b7e1226055de1830adda6c10920
mayakota <KotaM0993@sduhsd.lan>
2017-04-28 10:20:44 -0700

    16

---
+++ semester 2/WebLesson_16/otherthing.js
@@ -0,0 +1,20 @@
+function mouse()
+{
+    var x = document.getElementById("canvas");
+    canvas = x.getContext("2d");
+
+    window.addEventListener("mousemove", icon, false);
+
+}
+
+function icon(e) {
+    canvas.clearRect(0, 0, 1000, 1000);
+    var xPos = e.clientX;
+    var yPos = e.clientY;
+    var pic = new Image();
+    pic.arc = "http://r.ddmcdn.com/s_f/o_1/cx_633/cy_0/cw_1725/ch_1725/w_720/APL/uploads/2014/11/too-cute-doggone-it-video-playlist.jpg";
+    canvas.drawImage(pic, xPos, yPos, 200, 200);
+
+}
+
+window.addEventListener("load", mouse, false);
